aa64047b0c0f3f1225ab5741be9f8a9fdf346d84
et11 <adrian.walczak119@gmail.com>
2023-12-05 16:23:56 +0000

    classyfing labels by avg of all annotations

--- visualize_data.py
+++ visualize_data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import csv
 
 patients_ids = []
-path = "C:\Pliki\sem5\Advanced_data_mining\Final_project\input\LIDC-01-10\manifest-1701190789220"
+path = "C:\Pliki\sem5\Advanced_data_mining\DEEPL-LIDC\input\LIDC-01-10\manifest-1701190789220"
 with open(path+"\metadata.csv", newline='', encoding='utf-8') as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
@@ -13,11 +13,37 @@ with open(path+"\metadata.csv", newline='', encoding='utf-8') as csvfile:
             patients_ids.append(subject_id)
 
 annotations_ids= [[] for _ in patients_ids]
+malignancy_avg = [0 for _ in patients_ids]
+
+
+def link_patients_with_annotations():
+    annot = pl.query(pl.Annotation)
+    for x in annot:
+        if x.scan.patient_id in patients_ids:
+            index = patients_ids.index(x.scan.patient_id)
+            annotations_ids[index].append(x.id)
+
+def visualize_by_pid(pid):
+    plt.figure(figsize=(3, 3))
+    plt.set_cmap('gray')
+    scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
+    vol = scan.to_volume()
+    nodules = scan.cluster_annotations()
+    scan.visualize(nodules)
+
+def calculate_malignancy_avg():
+    for i in range (len(patients_ids)):
+        sum=0
+        if annotations_ids[i]:
+            for j in annotations_ids[i]:
+                annot = pl.query(pl.Annotation).filter(pl.Annotation.id == j).first()
+                sum+= annot.malignancy
+            malignancy_avg[i]=sum/len(annotations_ids[i])
 
 
 def print_patients_and_annotations():
     for i in range (len(patients_ids)):
-        print(f'patient_ID: {patients_ids[i]}, annotations_ids: {annotations_ids[i]}')
+        print(f'patient_ID: {patients_ids[i]}, patient_malignancy_acg: {malignancy_avg[i]}, annotations_ids: {annotations_ids[i]}')
 
 def print_annotations_by_patient_index(k):
     print(f"Annotations linked to patient {patients_ids[k]}")
@@ -27,42 +53,21 @@ def print_annotations_by_patient_index(k):
  #         annot.print_formatted_feature_table()
 
 
-annot = pl.query(pl.Annotation)
+################# DATA PREPROCESSING
+link_patients_with_annotations()
+calculate_malignancy_avg()
 
-for x in annot:
-    if x.scan.patient_id in patients_ids:
-        index = patients_ids.index(x.scan.patient_id)
-        annotations_ids[index].append(x.id)
+##
+malignant_lower_bound = 2.5
+labels = [int(x>malignant_lower_bound) for x in malignancy_avg]
+print(labels)
+####################################
 
-
-'''
 print_patients_and_annotations()
-print_annotations_by_patient_index(1)
+print_annotations_by_patient_index(4)
+pid = 'LIDC-IDRI-0004'
+#visualize_by_pid()
 
 
 
 
-#Visualize 
-plt.figure(figsize=(3, 3))
-plt.set_cmap('gray')
-pid = 'LIDC-IDRI-0007'
-scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
-vol = scan.to_volume()
-nodules = scan.cluster_annotations()
-scan.visualize(nodules)
-
-'''
-
-
-pid = 'LIDC-IDRI-0001'
-for pid in patients_ids:
-    scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
-    
-    vol = scan.to_volume()
-    print(vol.shape) # (dim, dim, depth)
-
-    plt.figure(figsize=(5, 5))
-    plt.set_cmap('gray')
-
-    plt.imshow(vol[:,:,0])
-    plt.show() # show the first-level slice
